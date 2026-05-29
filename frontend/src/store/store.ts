55c71ffb5af4ade6d6251eeded12e051c12871a5
unknown <adarshkamble01@gmail.com>
2026-05-30 00:30:42 +0530

    add zustand store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,41 +1,11 @@
+import axios from "axios";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-
-interface GitUserStore {
-  user: any;
-  gitUserSet: (newUser: any) => void;
-}
-
-export const useGitUser = create<GitUserStore>((set) => ({
-  user: {},
-  gitUserSet: (newUser: any) => {
-    set(() => ({
-      user: newUser,
-    }));
-  },
-}));
-
-//---------------------------------------------------
-
-interface isLoadingState {
-  loadingState: boolean;
-  loadingStateSet: (newVal: any) => void;
-}
-
-export const useLoadingState = create<isLoadingState>((set) => ({
-  loadingState: false,
-  loadingStateSet: (newVal: any) => {
-    set(() => ({
-      loadingState: newVal,
-    }));
-  },
-}));
-
-//------------------------------------------------------
 
 interface repoState {
   repos: any;
   reposStateSet: (newVal: any) => void;
+  fetchRepo: (getAccessTokenSilently: any) => void;
 }
 
 export const useReposState = create<repoState>((set) => ({
@@ -45,6 +15,24 @@ export const useReposState = create<repoState>((set) => ({
       repos: newVal,
     }));
   },
+  fetchRepo: async (getAccessTokenSilently: any) => {
+    try {
+      const token = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: `${import.meta.env.VITE_AUTH0_API_AUDIENCE}`,
+        },
+      });
+      const res = await axios.get(`${import.meta.env.VITE_GET_REPOS}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      set({ repos: res.data });
+    } catch (e) {
+      console.log(e);
+    }
+  },
 }));
 
 //------------------------------------------------------
@@ -52,8 +40,8 @@ export const useReposState = create<repoState>((set) => ({
 interface AuthState {
   user: any | null;
   isAuthenticated: boolean;
-  // isLoading: boolean;
-
+  userID: any | null;
+  setUserId: (getAccessTokenSilently: any, user: any) => Promise<Number | string | null>;
   setUser: (user: any) => void;
   clearUser: () => void;
 }
@@ -63,6 +51,7 @@ export const useAuthStore = create<AuthState>()(
     (set) => ({
       user: null,
       isAuthenticated: false,
+      userID: null,
 
       setUser: (user) =>
         set({
@@ -70,10 +59,37 @@ export const useAuthStore = create<AuthState>()(
           isAuthenticated: true,
         }),
 
+      setUserId: async (getAccessTokenSilently, user) => {
+        try {
+          const token = await getAccessTokenSilently({
+            authorizationParams: {
+              audience: `${import.meta.env.VITE_AUTH0_API_AUDIENCE}`,
+            },
+          });
+          if (user) {
+            const res = await axios.get(`${import.meta.env.VITE_GET_USER}`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+              params: {
+                User: JSON.stringify(user),
+              },
+            });
+            const id = res.data.user.id;
+            set({ userID: id });
+            return id;
+          }
+          return null;
+        } catch (e) {
+          console.log(e);
+          return null;
+        }
+      },
       clearUser: () =>
         set({
           user: null,
           isAuthenticated: false,
+          userID: null,
         }),
     }),
     {
@@ -84,3 +100,53 @@ export const useAuthStore = create<AuthState>()(
     }
   )
 );
+
+//------------------------------------------------------
+
+interface ProjectDetail {
+  projectDetail: any | null;
+  setProjectDetail: (detail: any) => void;
+  clearProjectDetail: () => void;
+}
+
+export const useProjectDetail = create<ProjectDetail>((set) => ({
+  projectDetail: {},
+  setProjectDetail: (details: any) => {
+    set({ projectDetail: details });
+  },
+  clearProjectDetail: () => {
+    set({ projectDetail: null });
+  },
+}));
+
+//-------------------------------------------------------
+interface ProjectList {
+  projectList: any;
+  fetchProjectList: (getAccessTokenSilently: any, userId: any) => void;
+}
+
+export const userProjectList = create<ProjectList>((set) => ({
+  projectList: [],
+
+  fetchProjectList: async (getAccessTokenSilently: any, userId: any) => {
+    try {
+      const token = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: `${import.meta.env.VITE_AUTH0_API_AUDIENCE}`,
+        },
+      });
+      const res = await axios.get(`${import.meta.env.VITE_GET_PROJECTS}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          id: userId,
+        },
+      });
+
+      set({ projectList: res.data });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+}));
